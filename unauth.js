bb395922f5bf9bd2904ab35fb52dae8c7b348874
lbioum22 <105925874+lbioum22@users.noreply.github.com>
2022-05-21 16:01:52 +0100

    Update unauth.js
    Send unauth SDEs

--- unauth.js
+++ unauth.js
@@ -1,3 +1,19 @@
+//SDEs
+indow.lpTag = window.lpTag || {};
+lpTag.sdes = lpTag.sdes||[];
+lpTag.sdes.push(
+{
+  "type": "ctmrinfo",
+  "info": {
+    "customerId": "lpb_subsc1",
+    "balance": 11.7,
+    "currency": "USD",
+    "storeZipCode": "Shopping Cart JS Unauthenticated"
+  }
+
+}
+);
+
 //Identity function unauthorised
 lpTag.identities = [];
 lpTag.identities.push(identityFn);
