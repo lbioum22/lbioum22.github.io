cf0e770f2005b37ca6e9635b33515c2721ca19d8
lbioum22 <105925874+lbioum22@users.noreply.github.com>
2022-09-14 09:23:39 +0100

    Update unauth.js

--- unauth.js
+++ unauth.js
@@ -1,19 +1,19 @@
 //SDEs
 window.lpTag = window.lpTag || {};
 lpTag.sdes = lpTag.sdes||[];
-// lpTag.sdes.push(
-// {
-//   "type": "ctmrinfo",
-//   "info": {
-//     "customerId": "lpb_subsc1",
-//     "ctype": "nc_bss",
-//     "balance": 11.7,
-//     "currency": "USD",
-//     "storeZipCode": document.title
-//   }
+lpTag.sdes.push(
+{
+  "type": "ctmrinfo",
+  "info": {
+    "customerId": "lpb_subsc1",
+    "ctype": "nc_bss",
+    "balance": 11.7,
+    "currency": "USD",
+    "storeZipCode": document.title
+  }
 
-// }
-// );
+}
+);
 
 //Identity function unauthorised
 lpTag.identities = [];
